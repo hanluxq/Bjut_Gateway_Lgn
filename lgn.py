c3abdc229ad45d95bc66654c0bebbb036293b9bf
shuanglengyunji <liuhan211211@gmail.com>
2018-06-27 12:36:09 +0800

    完成登录检测

--- lgn.py
+++ lgn.py
@@ -1,8 +1,11 @@
 #coding=utf-8
 
 import requests
+import string
+
 from bs4 import BeautifulSoup
 
+# 检测网关是否登录，返回网关登录状态
 def lgn_test():
     url = "https://lgn.bjut.edu.cn/"
     r = requests.get(url, timeout=5)
@@ -10,17 +13,16 @@ def lgn_test():
     html = r.text   # 获取网页信息
     # print(html)   # 输出网页内容
     html = BeautifulSoup(html, 'html.parser')   # 用BeautifulSoup解析数据  python3 必须传入参数二'html.parser' 得到一个对象，接下来获取对象的相关属性
-    title = html.title   # 读取title内容
-    print(title)      # 输出title内容
-    if title == '<title>北京工业大学上网信息窗                  </title>':
-        print("1")
+    title = html.title      # 读取title内容
+    titlestr = str(title)   # 转换为string类型
+    # print(titlestr)
+    if titlestr == "<title>北京工业大学上网信息窗                  </title>":
         return True
     else:
-        print("2")
         return False
 
+# 登录网关，返回登录结果
 def lgn():
-    print("\n开始登录lgn网关\n")
     url = "https://lgn.bjut.edu.cn/"                    # 设置目标url
     postdata = {                                            # post携带的字符串
         'DDDDD': '14024210',    # 用户名
@@ -30,7 +32,15 @@ def lgn():
         'f4serip': '172.30.201.10',
         '0MKKey': ''
     }
-    r = requests.post(url, data=postdata, timeout=0.1)          # 向指定url发送post信息
+    r = requests.post(url, data=postdata, timeout=5)          # 向指定url发送post信息
     r.encoding = 'gb2312'
-    print("返回信息：\n",r.text,"\n")                         # 输出返回的信息
-    return True
+    html = r.text   # 获取网页信息
+    # print(html)   # 输出网页内容
+    html = BeautifulSoup(html, 'html.parser')   # 用BeautifulSoup解析数据  python3 必须传入参数二'html.parser' 得到一个对象，接下来获取对象的相关属性
+    title = html.title      # 读取title内容
+    titlestr = str(title)   # 转换为string类型
+    # print(titlestr)
+    if titlestr == "<title>登录成功窗</title>":
+        return True
+    else:
+        return False
--- main.py
+++ main.py
@@ -3,27 +3,28 @@
 import requests
 import lgn,connect_test
 
-'''
+
 # 测试网络连接情况（测试能否访问lgn网关）
 status = connect_test.test()
 if status == False:
     print("网关访问失败\n")
     exit(0)     # 无法连接到lgn网关，程序停止运行
-'''
+else:
+    print("网关访问正常\n")
+
 
 # 检测是否登录
 status = lgn.lgn_test()
 if status == True:
-    print("网关已经登录")
+    print("网关已经登录\n")
     exit(0)                 # 自动登录程序停止运行
 else:
-    print("网关尚未登录") # 继续进行网关登录
+    print("网关尚未登录\n") # 继续进行网关登录
 
-'''
+
 # 登录lgn
 status = lgn.lgn()
 if status == False:
     print("网关登录失败\n")
 else:
     print("网关登录成功\n")
-'''
